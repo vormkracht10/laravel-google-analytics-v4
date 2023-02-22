023f27ef0055fc03c8f8175e40bcb9243898da54
Bas van Dinther <bas@vormkracht10.nl>
2023-02-22 10:53:48 +0100

    Add install command

--- src/AnalyticsServiceProvider.php
+++ src/AnalyticsServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace Vormkracht10\Analytics;
 
+use Spatie\LaravelPackageTools\Commands\InstallCommand;
 use Spatie\LaravelPackageTools\Package;
 use Spatie\LaravelPackageTools\PackageServiceProvider;
 use Vormkracht10\Analytics\Commands\AnalyticsCommand;
@@ -10,16 +11,17 @@ class AnalyticsServiceProvider extends PackageServiceProvider
 {
     public function configurePackage(Package $package): void
     {
-        /*
-         * This class is a Package Service Provider
-         *
-         * More info: https://github.com/spatie/laravel-package-tools
-         */
         $package
             ->name('laravel-google-analytics')
-            ->hasConfigFile()
-            ->hasViews()
-            ->hasMigration('create_laravel-google-analytics_table')
-            ->hasCommand(AnalyticsCommand::class);
+            ->hasConfigFile();
+
+        if (app()->runningUnitTests()) {
+            return; 
+        }
+
+        $package->hasInstallCommand(function (InstallCommand $command) {
+            $command->publishConfigFile()
+                ->askToStarRepoOnGitHub('vormkracht10/laravel-google-analytics');
+        });
     }
 }
