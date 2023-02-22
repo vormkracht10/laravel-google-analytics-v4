6ce61ad43def400514e2bc5f1eaed2c921ad7de6
Bas van Dinther <bas@vormkracht10.nl>
2023-02-22 10:39:13 +0100

    Add config and getters / setters

--- src/Analytics.php
+++ src/Analytics.php
@@ -2,6 +2,47 @@
 
 namespace Vormkracht10\Analytics;
 
+use Google\Analytics\Data\V1beta\BetaAnalyticsDataClient;
+
 class Analytics
 {
+    public ?int $propertyId = null;
+    public ?string $credentials = null;
+
+    public function __construct()
+    {
+        $this->propertyId = config('google-analytics.property_id') ?? null;
+        $this->credentials = config('google-analytics.credentials_json') ?? null;
+    }
+
+    public function setPropertyId(int $propertyId): self
+    {
+        $this->propertyId = $propertyId;
+
+        return $this;
+    }
+
+    public function setCredentials(string $credentials): self
+    {
+        $this->credentials = $credentials;
+
+        return $this;
+    }
+
+    public function getCredentials(): ?string
+    {
+        return $this->credentials;
+    }
+
+    public function getPropertyId(): ?int
+    {
+        return $this->propertyId;
+    }
+
+    public function getClient(): BetaAnalyticsDataClient
+    {
+        return new BetaAnalyticsDataClient([
+            'credentials' => $this->getCredentials(),
+        ]);
+    }
 }
