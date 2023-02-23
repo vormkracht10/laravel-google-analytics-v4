972ee46108055aa5166cfe934d377c9c09f2c5fe
Bas van Dinther <bas@vormkracht10.nl>
2023-02-23 11:39:04 +0100

    Add more user metric endpoints

--- src/Traits/Entities/Users.php
+++ src/Traits/Entities/Users.php
@@ -3,6 +3,7 @@
 namespace Vormkracht10\Analytics\Traits\Entities;
 
 use Illuminate\Support\Arr;
+use Vormkracht10\Analytics\Enums\Direction;
 use Vormkracht10\Analytics\Period;
 
 trait Users
@@ -19,5 +20,57 @@ trait Users
             ->dataTable;
 
         return (int) Arr::first(Arr::flatten($result));
+    }
+
+    /**
+     * @throws \Google\ApiCore\ApiException
+     * @throws \Google\ApiCore\ValidationException
+     */
+    public function getTotalUsersByDate(Period $period): array
+    {
+        return $this->setDateRange($period)
+            ->addMetrics('totalUsers')
+            ->addDimensions('date')
+            ->getReport()
+            ->dataTable;
+    }
+
+    /**
+     * @throws \Google\ApiCore\ApiException
+     * @throws \Google\ApiCore\ValidationException
+     */
+    public function getTotalUsersBySessionSource(Period $period): array
+    {
+        return $this->setDateRange($period)
+            ->addMetrics('totalUsers')
+            ->addDimensions('sessionSource')
+            ->getReport()
+            ->dataTable;
+    }
+
+    /**
+     * @throws \Google\ApiCore\ApiException
+     * @throws \Google\ApiCore\ValidationException
+     */
+    public function getTotalUsersBySessionMedium(Period $period): array
+    {
+        return $this->setDateRange($period)
+            ->addMetrics('totalUsers')
+            ->addDimensions('sessionMedium')
+            ->getReport()
+            ->dataTable;
+    }
+
+    /**
+     * @throws \Google\ApiCore\ApiException
+     * @throws \Google\ApiCore\ValidationException
+     */
+    public function getTotalUsersBySessionDevice(Period $period): array
+    {
+        return $this->setDateRange($period)
+            ->addMetrics('totalUsers')
+            ->addDimensions('sessionDevice')
+            ->getReport()
+            ->dataTable;
     }
 }
