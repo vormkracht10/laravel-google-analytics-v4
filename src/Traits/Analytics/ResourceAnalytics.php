7e29489f29434c2043fd69cb3550b8bfc51a632e
Baspa <Baspa@users.noreply.github.com>
2023-02-23 14:30:54 +0000

    Fix styling

--- src/Traits/Analytics/ResourceAnalytics.php
+++ src/Traits/Analytics/ResourceAnalytics.php
@@ -1,10 +1,6 @@
-<?php 
+<?php
 
 namespace Vormkracht10\Analytics\Traits\Analytics;
-
-use Illuminate\Support\Arr;
-use Vormkracht10\Analytics\Enums\Direction;
-use Vormkracht10\Analytics\Period;
 
 trait ResourceAnalytics
 {
