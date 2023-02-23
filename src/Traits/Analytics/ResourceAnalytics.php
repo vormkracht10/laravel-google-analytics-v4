5b72323208fa39fe173f4b3fd5ab7fe36fa28932
Bas van Dinther <bas@vormkracht10.nl>
2023-02-23 15:30:10 +0100

    Add Resource analytics skeleton class

---
+++ src/Traits/Analytics/ResourceAnalytics.php
@@ -0,0 +1,12 @@
+<?php 
+
+namespace Vormkracht10\Analytics\Traits\Analytics;
+
+use Illuminate\Support\Arr;
+use Vormkracht10\Analytics\Enums\Direction;
+use Vormkracht10\Analytics\Period;
+
+trait ResourceAnalytics
+{
+    // Get the top social media channels as  the source of traffic
+}
