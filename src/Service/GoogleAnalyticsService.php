8265e7aee33cd9347125e0ef601e7db2a712fae5
Baspa <Baspa@users.noreply.github.com>
2023-02-23 14:34:06 +0000

    Fix styling

--- src/Service/GoogleAnalyticsService.php
+++ src/Service/GoogleAnalyticsService.php
@@ -2,14 +2,14 @@
 
 namespace Vormkracht10\Analytics\Service;
 
-use Vormkracht10\Analytics\Traits\Google\MetricTrait;
-use Vormkracht10\Analytics\Traits\Google\OrderByTrait;
-use Vormkracht10\Analytics\Traits\Google\FilterByTrait;
 use Vormkracht10\Analytics\Traits\Google\DateRangeTrait;
 use Vormkracht10\Analytics\Traits\Google\DimensionTrait;
+use Vormkracht10\Analytics\Traits\Google\FilterByTrait;
+use Vormkracht10\Analytics\Traits\Google\MetricAggregationTrait;
+use Vormkracht10\Analytics\Traits\Google\MetricTrait;
+use Vormkracht10\Analytics\Traits\Google\OrderByTrait;
 use Vormkracht10\Analytics\Traits\Google\RowConfigTrait;
 use Vormkracht10\Analytics\Traits\ResponseFormatterTrait;
-use Vormkracht10\Analytics\Traits\Google\MetricAggregationTrait;
 
 class GoogleAnalyticsService
 {
