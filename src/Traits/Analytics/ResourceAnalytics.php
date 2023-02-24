d45c7c94ab4e2bf50890b56a5e73aac80d01c39b
Bas van Dinther <bas@vormkracht10.nl>
2023-02-24 16:34:09 +0100

    Get top refererers

--- src/Traits/Analytics/ResourceAnalytics.php
+++ src/Traits/Analytics/ResourceAnalytics.php
@@ -7,83 +7,66 @@ use Vormkracht10\Analytics\Period;
 
 trait ResourceAnalytics
 {
-    // Gett all traffic sources (organic, direct, referral, social, email)
-    public function getTrafficSources(Period $period): array
+    public function getTopReferrers(Period $period, int $limit): array
     {
         $googleAnalytics = $this->googleAnalytics
             ->setDateRange($period)
             ->addMetrics('sessions')
-            ->addDimensions('sessionSource')
-            ->orderByMetric('sessions', Direction::DESC);
+            ->addDimensions('pageReferrer')
+            ->orderByMetric('sessions', Direction::DESC)
+            ->limit($limit);
+
+        return $this->getReport($googleAnalytics)
+            ->dataTable;
+    }
+
+    public function getLandingPages(Period $period, int $limit): array
+    {
+        $googleAnalytics = $this->googleAnalytics
+            ->setDateRange($period)
+            ->addMetrics('sessions')
+            ->addDimensions('landingPagePath')
+            ->orderByMetric('sessions', Direction::DESC)
+            ->limit($limit);
 
         return $this->getReport($googleAnalytics)
             ->dataTable;
     }
 
 
-    public function getTopReferrers($days = 365, $maxResults = 10, $filters = [])
-    {
-        $data = [];
+    //  public function getLandingPages($days = 30, $maxResults = 10)
+    // {
+    //     $data = [];
+    //     $total = 0;
 
-        [$startDate, $endDate] = $this->getStartEndDate($days);
+    //     [$startDate, $endDate] = $this->getStartEndDate($days);
 
-        $params = [
-            'dimensions' => 'ga:fullReferrer',
-            'sort' => '-ga:sessions',
-            'max-results' => $maxResults,
-        ];
+    //     $params = [
+    //         'dimensions' => 'ga:landingPagePath',
+    //         'sort' => '-ga:pageviews',
+    //         'filters' => 'ga:landingPagePath!=/',
+    //         'max-results' => $maxResults,
+    //     ];
 
-        if ($filters) {
-            $params['filters'] = $filters;
-        }
+    //     $query = $this->performQuery($startDate, $endDate, 'ga:pageviews', $params);
 
-        $query = $this->performQuery($startDate, $endDate, 'ga:sessions', $params);
+    //     if ($query->rows) {
+    //         foreach ($query->rows as $row) {
+    //             $data[] = [
+    //                 'path' => '/'.ltrim($row[0], '/'),
+    //                 'pageviews' => $row[1],
+    //             ];
 
-        if ($query->rows) {
-            foreach ($query->rows as $row) {
-                $data[] = [
-                    'url' => rtrim($row[0], '/'),
-                    'sessions' => $row[1],
-                ];
-            }
-        }
+    //             $total += $row[1];
+    //         }
 
-        return collect($data);
-    }
+    //         foreach ($data as $i => $row) {
+    //             $data[$i]['percent'] = round($row['pageviews'] / $total * 100, 0);
+    //         }
+    //     }
 
-     public function getLandingPages($days = 30, $maxResults = 10)
-    {
-        $data = [];
-        $total = 0;
-
-        [$startDate, $endDate] = $this->getStartEndDate($days);
-
-        $params = [
-            'dimensions' => 'ga:landingPagePath',
-            'sort' => '-ga:pageviews',
-            'filters' => 'ga:landingPagePath!=/',
-            'max-results' => $maxResults,
-        ];
-
-        $query = $this->performQuery($startDate, $endDate, 'ga:pageviews', $params);
-
-        if ($query->rows) {
-            foreach ($query->rows as $row) {
-                $data[] = [
-                    'path' => '/'.ltrim($row[0], '/'),
-                    'pageviews' => $row[1],
-                ];
-
-                $total += $row[1];
-            }
-
-            foreach ($data as $i => $row) {
-                $data[$i]['percent'] = round($row['pageviews'] / $total * 100, 0);
-            }
-        }
-
-        return collect($data);
-    }
+    //     return collect($data);
+    // }
 
     public function getExitPages($days = 30, $maxResults = 10)
     {
