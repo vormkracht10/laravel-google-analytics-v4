1cd3f682f61176cd044398bd4e3cfacb36e50285
Bas van Dinther <bas@vormkracht10.nl>
2023-03-29 14:33:02 +0200

    Use pagepath

--- src/Traits/Analytics/ViewsAnalytics.php
+++ src/Traits/Analytics/ViewsAnalytics.php
@@ -134,7 +134,7 @@ trait ViewsAnalytics
     {
         $googleAnalytics = $this->googleAnalytics->setDateRange($period)
             ->addMetrics('screenPageViews')
-            ->addDimensions('pageTitle', 'pageReferrer')
+            ->addDimensions('pageTitle', 'pagePath')
             ->orderByMetric('screenPageViews', Direction::DESC)
             ->limit($limit);
 
