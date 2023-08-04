24adbf29945632155a6374e5507c3d139b2ee5ea
Bas van Dinther <bas@vormkracht10.nl>
2023-08-04 10:48:52 +0200

    wip

--- src/Traits/Analytics/RealtimeAnalytics.php
+++ src/Traits/Analytics/RealtimeAnalytics.php
@@ -11,7 +11,7 @@ trait RealtimeAnalytics
      * @throws \Google\ApiCore\ApiException
      * @throws \Google\ApiCore\ValidationException
      */
-    public function activeUsers(Period $period = null, string $path = null): int
+    public function activeUsers(Period $period = null, string $path = null): int|array
     {
         if (is_null($period)) {
             $period = Period::minutes(30);
